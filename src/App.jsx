d92e987dc5c1952be8d94f31a07b569e24cecb80
joeemilio <joeemilio22@gmail.com>
2024-08-16 19:52:34 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Mensaje1 from './Mensaje1'
+
+const App = ()=> { 
+  <br />
+  const mensaje = "ola"
+  return (
+    <>
+      <div>
+        <div><Mensaje1 /></div>
+        
+        {mensaje + '  mundo mundo'}
+      </div>
+      
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Mensaje1.jsx
@@ -0,0 +1,4 @@
+const Mensaje1 =() =>{
+return("mi primer mensaje")
+}
+export default Mensaje1
